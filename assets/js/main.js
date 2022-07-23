2dd1be09fe2a4622c442b5924458e8e70755516a
Đỗ Chí Hưng <61616410+dochihung2909@users.noreply.github.com>
2022-07-23 21:04:25 +0700

    fix back to top btn
    fix back to top btn

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,10 @@
 
 // Scroll
 
-window.addEventListener('scroll', function () { 
-    const header = document.querySelector('.header');
-    header.classList.toggle("sticky", window.scrollY > 80);
-})
+// window.addEventListener('scroll', function () { 
+//     const header = document.querySelector('.header');
+//     header.classList.toggle("sticky", window.scrollY > 80);
+// })
 
 
 window.addEventListener('scroll', function () { 
