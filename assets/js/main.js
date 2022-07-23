b7e88713fa0ecd07df180a6a233161d18579816c
Đỗ Chí Hưng <61616410+dochihung2909@users.noreply.github.com>
2022-07-23 20:51:58 +0700

    fix trailer slide
    fix trailer slide and add back to top btn

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,9 @@ window.addEventListener('scroll', function () {
     const header = document.querySelector('.header');
     header.classList.toggle("sticky", window.scrollY > 80);
 })
+
+
+window.addEventListener('scroll', function () { 
+    const header = document.querySelector('.back-to-top');
+    header.classList.toggle("top", window.scrollY > 120);
+})
