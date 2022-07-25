a5c3c7fa639c31633deab453dce8c3328d1dc236
ngHungOU <2151053024hung@ou.edu.vn>
2022-07-25 13:02:25 +0700

    modal buy ticket
    modal buy ticket

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,33 @@
 
 // Scroll
 
+
+
 // window.addEventListener('scroll', function () { 
 //     const header = document.querySelector('.header');
 //     header.classList.toggle("sticky", window.scrollY > 80);
 // })
 
-
+// Header scroll
 window.addEventListener('scroll', function () { 
     const header = document.querySelector('.back-to-top');
     header.classList.toggle("top", window.scrollY > 120);
 })  
+
+
+// Trailer view 
 
 function viewTrailerJs(filmName, urlTrailer) {  
     document.getElementById("film-name").innerHTML += filmName;  
     document.getElementById("trailer").innerHTML = '<iframe id="fancybox-frame" width="100%" height="500px" src="' + urlTrailer.replace("watch?v=", "embed/") + '?rel=0&amp;showinfo=0&amp;autoplay=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>'
 }
 
+// Reset input trailer modal
+
 function resetViewTrailer() {
     document.getElementById("film-name").innerHTML = "";  
     document.getElementById("trailer").innerHTML = "";
-}
-
-
+} 
 
 
 // Modal
@@ -31,27 +36,75 @@ function resetViewTrailer() {
     const modalclose = document.querySelector('.js-modal-close')
     const modalcontainer = document.querySelector('.js-modal-container')
 
-    // Hàm hiển thị modal mua vé (Thêm class open vào modal)
-    function showBuyTickets () {
-        modal.classList.add("open")
+    const buyTickets = document.querySelectorAll('.buy-btn');
+    const modal_buy = document.querySelector('.js-modal-buy-ticket');
+    const modalCloseBuyTicket = document.querySelector('.js-modal-buy-ticket-close');
+    const modalBuyContainer = document.querySelector('.js-modal-buy-ticket-container');
+
+
+    function openModal(btns,modal,modalClose,modalContainer,reset) {
+        // Hàm hiển thị modal mua vé (Thêm class open vào modal)
+        function showBuyTickets () {
+            modal.classList.add("open")
+        }
+    
+        // Hàm ẩn modal mua vé (gỡ bỏ class open vào modal)
+        function hidenBuyTickets () {
+            modal.classList.remove("open"); 
+            // Reset
+            reset();
+        }
+    
+        // Lặp qua từng thẻ button và nghe hành vi click
+        for (const buyBtn of btns) {
+            buyBtn.addEventListener('click', showBuyTickets)
+        }
+    
+        // Nghe hành vi click vào button close
+        modalClose.addEventListener('click', hidenBuyTickets)
+    
+        modal.addEventListener('click', hidenBuyTickets)
+    
+        modalContainer.addEventListener('click', function(event){ 
+        event.stopPropagation();
+        })
+    } 
+ 
+   
+    openModal(buyBtns,modal,modalclose,modalcontainer,resetViewTrailer);
+    openModal(buyTickets,modal_buy,modalCloseBuyTicket,modalBuyContainer);
+
+
+
+    var btns = document.querySelectorAll('.buy__time-wrapped');
+    for (const btn of btns) { 
+        btn.addEventListener('click', function() { 
+            let current = document.querySelector('.active-modal-buy');
+            current.classList.remove('active-modal-buy');
+            this.classList.add('active-modal-buy'); 
+        })
     }
+ 
+// Buy ticket modal
 
-    // Hàm ẩn modal mua vé (gỡ bỏ class open vào modal)
-    function hidenBuyTickets () {
-        modal.classList.remove("open");
-        resetViewTrailer();
-    }
+    // const buyTickets = document.querySelectorAll('.buy-btn');
+    // const modal_buy = document.querySelector('.js-modal-buy-ticket');
+    // const modalCloseBuyTicket = document.querySelector('.js.modal-buy-ticket-close');
+    // const modalBuyContainer = document.querySelector('.js-modal-buy-ticket-container');
+   
+    // openModal(buyTickets,modal_buy,modalCloseBuyTicket,modalBuyContainer);
+    
 
-    // Lặp qua từng thẻ button và nghe hành vi click
-    for (const buyBtn of buyBtns) {
-        buyBtn.addEventListener('click', showBuyTickets)
-    }
+// document.addEventListener('DOMContentLoaded', () => {
 
-    // Nghe hành vi click vào button close
-    modalclose.addEventListener('click', hidenBuyTickets)
+//     let myBtns=document.querySelectorAll('.buy__time-wrapped');
+//     myBtns.forEach(function(btn) {
 
-    modal.addEventListener('click', hidenBuyTickets)
+//         btn.addEventListener('click', () => {
+//             myBtns.forEach(b => b.classList.remove('active'));
+//             btn.classList.add('active');
+//         });
+    
+//     });
 
-    modalcontainer.addEventListener('click', function(event){ 
-    event.stopPropagation();
-    })
+// });
