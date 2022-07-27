cdf9850c1e64c8b5e18458fe05a03745e0996055
Đỗ Chí Hưng <61616410+dochihung2909@users.noreply.github.com>
2022-07-28 00:12:08 +0700

    fix js add modal buy
    fix js page 1 add modal buy to index

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,20 +25,19 @@ function resetViewTrailer() {
 } 
 
 
-// Modal
-
+// Modal 
     // Var of modal trailer
     const buyBtns = document.querySelectorAll('.film-trailer-item') 
     const modal = document.querySelector('.js-modal')
     const modalclose = document.querySelector('.js-modal-close')
-    const modalcontainer = document.querySelector('.js-modal-container')
+    const modalcontainer = document.querySelector('.js-modal')
 
 
     // Var of modal buy tickets
     const buyTickets = document.querySelectorAll('.buy-btn');
     const modal_buy = document.querySelector('.js-modal-buy-ticket');
     const modalCloseBuyTicket = document.querySelector('.js-modal-buy-ticket-close');
-    const modalBuyContainer = document.querySelector('.js-modal-buy-ticket-container');
+    const modalBuyContainer = document.querySelector('.js-modal-buy-ticket');
 
 
     var btns = document.querySelectorAll('.buy__time-wrapped');
@@ -66,24 +65,70 @@ function resetViewTrailer() {
     
         // Lặp qua từng thẻ button và nghe hành vi click
         for (const buyBtn of btns) {
-            buyBtn.addEventListener('click', showBuyTickets)
+            buyBtn.addEventListener('click', showBuyTickets,false)
         }
     
         // Nghe hành vi click vào button close
-        modalClose.addEventListener('click', hidenBuyTickets)
-    
-        modal.addEventListener('click', hidenBuyTickets)
+        modalClose.addEventListener('click', hidenBuyTickets,false);
+        
+        modal.addEventListener('click', hidenBuyTickets); 
     
         modalContainer.addEventListener('click', function(event){ 
-        event.stopPropagation();
+            event.stopPropagation();
         })
     }  
 
 
     // Modal trailer
-    openModal(buyBtns,modal,modalclose,modalcontainer,resetViewTrailer);
-
-
+    if (modal) {
+        openModal(buyBtns,modal,modalclose,modalcontainer,resetViewTrailer);
+    }
+    
     // Modal buy tickets
-    openModal(buyTickets,modal_buy,modalCloseBuyTicket,modalBuyContainer);
+    if (modal_buy) {
+        openModal(buyTickets,modal_buy,modalCloseBuyTicket,modalBuyContainer,false);
+    }
  
+
+let dayBtns = document.querySelectorAll('.buy__time-day');
+let dateBtns = document.querySelectorAll('.buy__time-date');
+  
+
+function changeDay(days,dates) {
+    let i = 1; 
+    let current = new Date();
+    let day = current.getDay();
+    let month = current.getMonth() + 1; 
+    let date = current.getDate(); 
+    for (let dayBtn of days) {
+        day++;
+        if (day == 8) {
+            dayBtn.innerHTML = "Chủ Nhật";  
+            day = 1;
+        }
+        else {
+            dayBtn.innerHTML = "Thứ " + day;
+        }
+    } 
+    const month31 = [1,3,5,7,8,10,12];
+    const month30 = [4,6,9,11];
+    for (let dateBtn of dates) {  
+        if (date == 32 && month31.includes(month)) {
+            date = 1;
+            if (month == 12) 
+                month = 1;
+            else 
+                month++;
+        } else if (date == 31 && month30.includes(month)) {
+            date = 1;
+            month++;
+        } else if (month == 2 && date == 29) { 
+            month++;
+            date = 1;
+        }  
+        dateBtn.innerHTML = (date++) + "-" + (month);
+        
+    }
+}
+
+changeDay(dayBtns,dateBtns);
