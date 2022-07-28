a079a86e3fee3c537ef46b4b75988abf3e879ee3
Đỗ Chí Hưng <61616410+dochihung2909@users.noreply.github.com>
2022-07-28 17:47:56 +0700

    js and slider
    add js and slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -132,3 +132,11 @@ function changeDay(days,dates) {
 }
 
 changeDay(dayBtns,dateBtns);
+
+
+// Set rank
+
+const ranks = document.querySelectorAll('.film__item-rank');
+for (let i = 0;i < ranks.length;i++) {
+    ranks[i].innerText = i+1;
+}
