bad378b49ee48a8c6ec805bbf9cdf07146e2fdbf
Đỗ Chí Hưng <61616410+dochihung2909@users.noreply.github.com>
2022-08-04 23:35:55 +0700

    fix + add
    trailer modal + fix image film booking + add js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,69 @@
 // Trailer view  
-function viewTrailerJs(filmName, urlTrailer) {  
-    document.getElementById("film-name").innerHTML += filmName;  
-    document.getElementById("trailer").innerHTML = '<iframe id="fancybox-frame" width="100%" height="500px" src="' + urlTrailer.replace("watch?v=", "embed/") + '?rel=0&amp;showinfo=0&amp;autoplay=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>'
+function viewTrailerJs(urlTrailer) {   
+    document.getElementById("trailer").innerHTML = '<iframe id="fancybox-frame" width="100%" height="650px" src="' + urlTrailer.replace("watch?v=", "embed/") + '?rel=0&amp;showinfo=0&amp;autoplay=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>'
 }
 
 // Reset input trailer modal 
-function resetViewTrailer() {
-    document.getElementById("film-name").innerHTML = "";  
+function resetViewTrailer() { 
     document.getElementById("trailer").innerHTML = "";
-} 
+}  
+
+// Redirect Page jQuery 
+function redirectPage(className,pageUrl) {
+    $(document).ready(function() {
+        $('.' + className).bind('click', function() {
+            window.location.replace(pageUrl);
+        });
+    });
+}
 
 
+// Open modal   
+function openModal(btns,modal,modalClose,modalContainer,reset) {
+    // Hàm hiển thị modal mua vé (Thêm class open vào modal)
+    function showBuyTickets () {
+        modal.classList.add("open")
+    }
+
+    // Hàm ẩn modal mua vé (gỡ bỏ class open vào modal)
+    function hidenBuyTickets () {
+        modal.classList.remove("open"); 
+        // Reset
+        reset();
+    }
+
+    // Lặp qua từng thẻ button và nghe hành vi click
+    for (const buyBtn of btns) {
+        buyBtn.addEventListener('click', showBuyTickets)
+    }
+
+    // Nghe hành vi click vào button close
+    modalClose.addEventListener('click', hidenBuyTickets);
+    
+    modal.addEventListener('click', hidenBuyTickets); 
+
+    modalContainer.addEventListener('click', function(event){ 
+        event.stopPropagation();
+    })
+}  
+
+// Set active rank
+function setActiveRank() {
+    $('.ranking__number-link').click(function() {
+        let current = $('.ranking-active');
+        current.removeClass('ranking-active');
+        $(this).addClass('ranking-active');
+    })
+}
+
+
+// Set image film
+function setSrcFilmItem() {
+    let film = $('.film-booking > .film__item-img');
+    film.attr('src','./assets/img/film_1.webp');
+}
+
+$(document).ready(function () {    
 // Modal 
     // Var of modal trailer
     const buyBtns = document.querySelectorAll('.film-trailer-item') 
@@ -25,57 +78,20 @@ function resetViewTrailer() {
     const modalCloseBuyTicket = document.querySelector('.js-modal-buy-ticket-close');
     const modalBuyContainer = document.querySelector('.js-modal-buy-ticket-container');
 
-    // Open modal
-    function openModal(btns,modal,modalClose,modalContainer,reset) {
-        // Hàm hiển thị modal mua vé (Thêm class open vào modal)
-        function showBuyTickets () {
-            modal.classList.add("open")
-        }
-    
-        // Hàm ẩn modal mua vé (gỡ bỏ class open vào modal)
-        function hidenBuyTickets () {
-            modal.classList.remove("open"); 
-            // Reset
-            reset();
-        }
-    
-        // Lặp qua từng thẻ button và nghe hành vi click
-        for (const buyBtn of btns) {
-            buyBtn.addEventListener('click', showBuyTickets)
-        }
-    
-        // Nghe hành vi click vào button close
-        modalClose.addEventListener('click', hidenBuyTickets);
-        
-        modal.addEventListener('click', hidenBuyTickets); 
-    
-        modalContainer.addEventListener('click', function(event){ 
-            event.stopPropagation();
-        })
-    }  
-
-
     // Modal trailer
     if (modal) {
         openModal(buyBtns,modal,modalclose,modalcontainer,resetViewTrailer);
     }
-    
+
     // Modal buy tickets
     if (modal_buy) {
         openModal(buyTickets,modal_buy,modalCloseBuyTicket,modalBuyContainer,false);
     } 
 
+    redirectPage('buy__theatre-exactlyTime','./booking.html'); 
+    redirectPage('film-booking','./film.html');   
 
-    // Redirect Page jQuery
+    setActiveRank(); 
 
-    function redirectPage(className,pageUrl) {
-        $(document).ready(function() {
-            $('.' + className).bind('click', function(event) {
-                window.location.replace(pageUrl);
-            });
-        });
-    }
-
-    redirectPage('buy__theatre-exactlyTime','./booking.html');
-
-    redirectPage('film__item','./film.html');
+    setSrcFilmItem();
+}) 
