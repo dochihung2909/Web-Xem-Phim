db8b57878991c484c4b574609bbe304f91ac196c
Đỗ Chí Hưng <61616410+dochihung2909@users.noreply.github.com>
2022-08-05 23:26:05 +0700

    fix film page
    fix film page

--- assets/js/film.js
+++ assets/js/film.js
@@ -1,6 +1,25 @@
-
-// $(document).ready(function() {
-//     $('.buy__theatre-exactlyTime').bind('click', function(event) {
-//         window.location.replace("./booking.html");
-//     });
-// });
+$(document).ready(function() {
+    const date = $('.booking__date');
+    const day = $('.booking__day');
+    const month = $('.booking__month');
+ 
+    const d = new Date();
+    // date.text(`Thứ ${d.getDate() + 1}`);
+    
+    let i = d.getDate() + 1;
+    date.each(function() {   
+        let a = `Thứ ${i}`;
+        if (i == 8) {
+            a= "Chủ nhật";
+            i = 1;
+        }
+        $(this).text(a)
+        i++; 
+    })
+    i = d.getDay();
+    day.each(function() {
+        let a =`${i++}`;
+        $(this).text(a);
+    });
+    month.text(`Tháng ${d.getMonth() + 1}`); 
+}) 
