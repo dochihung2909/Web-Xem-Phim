d63e53d05906c35aa49d37770f981b42b51dfa8c
ngHungOU <2151053024hung@ou.edu.vn>
2022-08-18 22:15:36 +0700

    responsive story
    responsive story

---
+++ assets/js/responsive.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    let story = `
+    <div class="detail__title">
+                <p class="deltail__story">Cốt truyện</p>
+    </div>
+    `;
+    $(".detail__paragraph .col.l-4").append(story);
+});
